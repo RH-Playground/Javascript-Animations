3cd011392b1403c49a6f9d0e44b5fbfe17474fd2
Rasindu Hansika <rasinduhansika17218@gmail.com>
2023-06-01 17:30:51 +0530

    bounce back balls when hit by the cursor

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@ class Ballon {
     dy = Math.random() * 10 * (Math.random() < 0.5 ? -1 : 1);
     x = Math.random() * (innerWidth - 50);
     y = Math.random() * (innerHeight - 50);
-    r = Math.random() * 256;
-    g = Math.random() * 256;
-    b = Math.random() * 256;
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
     elm;
 
 
@@ -47,6 +47,35 @@ class Ballon {
     move() {
       this.x += this.dx;
       this.y += this.dy;
+
+      const r1 = divElm.offsetWidth / 2;
+      const r2 = 50 / 2;
+      const xDiff = this.x + r2 - divElm.offsetLeft;
+      const yDiff = this.y + r2 - divElm.offsetTop;
+      const hypot = Math.hypot(xDiff, yDiff);         
+
+      if (hypot <= (r1 + r2 + 5)){
+          const slope = Math.abs(yDiff / xDiff);
+          const angle = Math.abs(Math.atan(slope));
+
+          let translateY = (r1 + r2 +3) * Math.sin(angle);
+          let translateX = (r1 + r2 +3 ) * Math.cos(angle);
+
+          if (xDiff > 0) {
+              this.x = divElm.offsetLeft + translateX - r2;
+          }else {
+              this.x = divElm.offsetLeft - translateX - r2;
+          }
+          if (yDiff > 0){
+              this.y = divElm.offsetTop + translateY - r2;
+          }else{
+              this.y = divElm.offsetTop - translateY - r2;
+          }
+          divElm.style.backgroundColor = `rgb(${this.r},${this.g},${this.b})`;
+          this.dy = -this.dy;
+          this.dx = -this.dx;
+      }  
+
       if (this.x >= innerWidth - 50 || this.x <= 0) this.dx = -this.dx;
       if (this.y >= innerHeight - 50 || this.y <= 0) this.dy = -this.dy;
       this.elm.style.left = `${this.x}px`;
